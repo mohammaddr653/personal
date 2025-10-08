ccf401113658914aae0c597bba5c58aaa14b169a
Ozone <mohammaddr653@gmail.com>
2025-10-09 00:18:00 +0330

    v16-footer completed

--- front-end/src/components/common/footer.tsx
+++ front-end/src/components/common/footer.tsx
@@ -1,21 +1,40 @@
 import { useDarkStore } from "@/store";
 import Image from "next/image";
+import Link from "next/link";
+import DownloadSvg from "../icons/download-svg";
+import SocialIcons from "./social-icons";
 
 const Footer = () => {
   const { darkmode, setDarkmode } = useDarkStore();
 
   return (
     <footer className="mt-auto">
-      <div className="footer-container bg-[url(/2147734183.jpg)] dark:bg-[url(/417214515_fb463481-61a6-4dde-9787-66a180fa208c.jpg)] bg-cover bg-center bg-[#ffffffb3] dark:bg-[#000000bd] bg-blend-lighten dark:bg-blend-darken px-5 md:px-40 text-neutral-800 dark:text-neutral-200">
-        <div className="flex flex-col h-100 w-full overflow-y-hidden">
-          <div className="mt-auto">
-            <Image
-              src={darkmode ? "/derakhshande-dark.png" : "/derakhshande.png"}
-              alt="تایپوگرافی"
-              width={1906}
-              height={133}
-            ></Image>
+      <div className="footer-container pt-20 flex flex-col gap-15 overflow-y-hidden bg-[url(/2147734183.jpg)] dark:bg-[url(/417214515_fb463481-61a6-4dde-9787-66a180fa208c.jpg)] bg-cover bg-center bg-[#ffffffb3] dark:bg-[#000000bd] bg-blend-lighten dark:bg-blend-darken px-5 md:px-40 text-neutral-800 dark:text-neutral-200">
+        <div className="flex flex-col gap-6 items-center text-neutral-600 dark:text-neutral-200">
+          <h5 className="text-lg">برای دریافت فایل کامل رزومه کلیک کنید</h5>
+          <Link
+            href={"/CV-Amin Derakhshande.pdf"}
+            download={true}
+            className="bg-gradient-to-r flex flex-row-reverse justify-center items-end gap-1 hover:animate-pulse from-primary to-secondary border-2 border-white outline-2 outline-primary dark:outline-secondary p-3 px-4 rounded-lg text-neutral-50 text-shadow-2xs"
+          >
+            <DownloadSvg width={30} fill={"currentColor"}></DownloadSvg>
+            CV-Derakhshande
+          </Link>
+          <div className="flex mt-auto mb-5 gap-3">
+            <SocialIcons
+              width={"20px"}
+              height={"20px"}
+              fill={"currentColor"}
+            ></SocialIcons>
           </div>
+        </div>
+        <div className="mt-auto">
+          <Image
+            src={darkmode ? "/derakhshande-dark.png" : "/derakhshande.png"}
+            alt="تایپوگرافی"
+            width={1906}
+            height={133}
+          ></Image>
         </div>
       </div>
     </footer>
