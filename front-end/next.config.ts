e8777e11892d6e436f789f27cf2118d7836cd766
Ozone <mohammaddr653@gmail.com>
2025-10-15 01:42:18 +0330

    v25-change in next.config.ts file . changed to .js

--- front-end/next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  outputFileTracingRoot: __dirname,  //برای اینکه فایل package-lock.json همین دایرکتوری را در نظر بگیرد
-};
-
-export default nextConfig;
